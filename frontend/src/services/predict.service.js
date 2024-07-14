0ee2eb3e980d2797921c11a5adfea1f3b95a3a4e
imxuan03 <txuan0164@gmail.com>
2024-07-14 20:34:50 +0700

    final commit

--- frontend/src/services/predict.service.js
+++ frontend/src/services/predict.service.js
@@ -4,11 +4,6 @@ class PredictService {
     constructor(baseUrl = "/api/detect/") {
         this.api = createApiClient(baseUrl);
     }
-    async getAll() {
-        const respone = (await this.api.get("/")).data;
-        // console.log("service", respone)
-        return respone;
-    }
     async uploadImage(data) {
         const respone = await this.api.post("/", data);
         console.log(respone.data);
